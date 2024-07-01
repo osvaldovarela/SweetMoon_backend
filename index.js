f532ab524d838c0a4f7d2a4c575ecdd302a24b8a
Jorge Osvaldo Varela <94763025+osvaldovarela@users.noreply.github.com>
2024-07-01 18:46:39 -0300

    fix deploy

--- index.js
+++ index.js
@@ -31,6 +31,4 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 8081;
 
-app.listen(PORT, () =>
-  console.log(`Servidor escuchando en http://localhost:${PORT}`)
-);
+app.listen(PORT, () => console.log(`https://sweet-moon-backend.vercel.app/`));
